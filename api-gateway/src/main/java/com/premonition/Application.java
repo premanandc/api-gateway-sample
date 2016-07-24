54660fa57beb51e0954eac1109191a96f8675d61
Premanand C <premanandc@gmail.com>
2016-07-24 09:55:39 -0400

    Correctly working with concurrent line item processing

--- api-gateway/src/main/java/com/premonition/Application.java
+++ api-gateway/src/main/java/com/premonition/Application.java
@@ -1,17 +1,20 @@
 package com.premonition;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.premonition.web.Message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.util.ExceptionUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.Netty4ClientHttpRequestFactory;
 import org.springframework.integration.dsl.IntegrationFlow;
-import org.springframework.integration.dsl.channel.MessageChannels;
 import org.springframework.integration.dsl.http.Http;
 import org.springframework.integration.gateway.MessagingGatewaySupport;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.Executor;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
 import static java.util.stream.Collectors.joining;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -21,10 +24,18 @@ import static org.springframework.integration.dsl.support.Transformers.fromJson;
 @SpringBootApplication
 public class Application {
 
+    private static final String SHOPPING_CART_CHANNEL = "shoppingCartChannel";
+    private static final String ADD_LINE_ITEM_CHANNEL = "addLineItemChannel";
+    private static final String CHECKOUT_CHANNEL = "checkoutChannel";
+    private static final int THREADS = Runtime.getRuntime().availableProcessors() + 1;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    static {
+        ExceptionUtils.setFail(true);
+    }
     @Bean
     public MessagingGatewaySupport inboundGateway() {
         return Http.inboundGateway("/checkout")
@@ -33,7 +44,7 @@ public class Application {
                                 .consumes(APPLICATION_JSON_VALUE)
                                 .produces(APPLICATION_JSON_VALUE)
                 )
-                .requestChannel(inputChannel())
+                .requestChannel("inputChannel")
                 .get();
     }
 
@@ -43,63 +54,51 @@ public class Application {
     }
 
     @Bean
-    public MessageChannel inputChannel() {
-        return MessageChannels.direct().get();
-    }
-
-    @Bean
-    public IntegrationFlow fullCheckoutFlow() {
-        return f -> f.channel(inputChannel())
+    public IntegrationFlow fullCheckoutFlow(Executor executor) {
+        return f -> f.channel("inputChannel")
                 .transform(fromJson(ShoppingCart.class))
-                .enrich(e -> e.requestChannel(shoppingCartChannel()))
+                .enrich(e -> e.requestChannel(SHOPPING_CART_CHANNEL))
                 .split(ShoppingCart.class, ShoppingCart::getLineItems)
-                .enrich(e -> e.requestChannel(addLineItemChannel()))
+                .channel(c -> c.executor(executor))
+                .enrich(e -> e.requestChannel(ADD_LINE_ITEM_CHANNEL))
                 .aggregate(aggregator -> aggregator
                         .outputProcessor(g -> g.getMessages()
                                 .stream()
                                 .map(m -> (LineItem) m.getPayload())
                                 .map(LineItem::getName)
                                 .collect(joining(", "))))
-                .enrich(e -> e.requestChannel(checkoutChannel()))
+                .enrich(e -> e.requestChannel(CHECKOUT_CHANNEL))
                 .<String>handle((p, h) -> Message.called("We have " + p + " line items!!"));
     }
 
     @Bean
-    public MessageChannel checkoutChannel() {
-        return MessageChannels.direct("checkout").get();
-    }
-
-    @Bean
-    public MessageChannel shoppingCartChannel() {
-        return MessageChannels.direct("shopping-cart").get();
-    }
-
-    @Bean
-    public MessageChannel addLineItemChannel() {
-        return MessageChannels.direct("addLineItem").get();
-    }
-
-    @Bean
     public IntegrationFlow createShoppingCart() {
-        return f -> f.channel(shoppingCartChannel())
+        return f -> f.channel(SHOPPING_CART_CHANNEL)
                 .handle(outboundGateway("http://localhost:8080/api/shopping-cart", restTemplate())
                         .httpMethod(POST)
                         .expectedResponseType(String.class));
     }
 
     @Bean
+    public IntegrationFlow checkout() {
+        return f -> f.channel(CHECKOUT_CHANNEL)
+                .handle(outboundGateway("http://localhost:8080/api/checkout", restTemplate())
+                        .httpMethod(POST)
+                        .expectedResponseType(String.class));
+    }
+
+    @Bean
     public IntegrationFlow addLineItem() {
-        return f -> f.channel(addLineItemChannel())
+        return f -> f.channel(ADD_LINE_ITEM_CHANNEL)
                 .handle(outboundGateway("http://localhost:8080/api/add-line-item", restTemplate())
                         .httpMethod(POST)
                         .expectedResponseType(String.class));
     }
 
     @Bean
-    public IntegrationFlow checkout() {
-        return f -> f.channel(checkoutChannel())
-                .handle(outboundGateway("http://localhost:8080/api/checkout", restTemplate())
-                        .httpMethod(POST)
-                        .expectedResponseType(String.class));
+    public Executor executor() {
+        return newFixedThreadPool(THREADS, new ThreadFactoryBuilder()
+                .setNameFormat("lineitems-pool")
+                .build());
     }
 }
